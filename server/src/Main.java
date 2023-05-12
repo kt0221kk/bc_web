aac6d23738e84fc9bb73d10850b65de34ebf52c4
kt0221kk <kt0221kk@gmail.com>
2023-05-12 16:36:38 +0900

    sqlの初期化とdbの接続プログラムの作成

--- server/src/Main.java
+++ server/src/Main.java
@@ -9,6 +9,21 @@ public class Main {
         String password = "2068686";
         connection = DriverManager.getConnection(url, user, password);
         System.out.println("登録成功");
+        PreparedStatement statement;
+        try{
+          String sql = "SELECT * from users";
+          statement = connection.prepareStatement(sql);
+          ResultSet resultSet = statement.executeQuery();
+          System.out.println(resultSet);
+          while(resultSet.next()){
+            int UserId = resultSet.getInt("id");
+            String UserName = resultSet.getString("name");
+            String UserPassword = resultSet.getString("password");
+            System.out.println("id:"+UserId+"  name:"+UserName+"  password:"+UserPassword);
+          }
+        }catch(SQLException e){
+          throw new RuntimeException("SQLの実行に成功しました");
+        }
       } catch (ClassNotFoundException e) {
         throw new RuntimeException("JDBCドライバの登録に失敗しました",e);
         // TODO: handle exception
