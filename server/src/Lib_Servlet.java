7ff74a0f1f02d5aa20f2436c21c9d152ba6e929f
kt0221kk <kt0221kk@gmail.com>
2023-05-15 15:39:31 +0900

    tomcat環境開発用のブランチ作成

---
+++ server/src/Lib_Servlet.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.servlet.*;
+@WebServlet("/hellotmo")
+public void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException{
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head></head>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
